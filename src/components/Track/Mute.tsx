35d5dec6d7f6180185a7d3c53a62a704cdf6b997
stevebarakat <stevebarakat@gmail.com>
2024-03-06 03:36:12 -0500

    solomute refactor

--- src/components/Track/Mute.tsx
+++ src/components/Track/Mute.tsx
@@ -1,12 +1,11 @@
 import { ToggleContext } from "@/machines/toggleMachine";
 import { Toggle } from "../Buttons";
+import { TrackContext } from "./trackMachine";
 
-type Props = {
-  id: string;
-  channel: Channel;
-};
+function Mute() {
+  const { id } = TrackContext.useSelector((state) => state.context.track);
+  const channel = TrackContext.useSelector((state) => state.context.channel);
 
-function Mute({ id, channel }: Props) {
   const { send } = ToggleContext.useActorRef();
   const isActive = ToggleContext.useSelector((state) => state.context.active);
 
--- src/components/Track/SoloMute.tsx
+++ src/components/Track/SoloMute.tsx
@@ -1,19 +1,13 @@
-import { TrackContext } from "@/components/Track/trackMachine";
 import { ToggleContext } from "@/machines/toggleMachine";
 import Solo from "./Solo";
 import Mute from "./Mute";
 
 function SoloMute() {
-  const { id } = TrackContext.useSelector((state) => state.context.track);
-  const channel = TrackContext.useSelector((state) => state.context.channel);
-
   return (
     <div className="flex gap8">
       <ToggleContext.Provider>
-        <Solo id={id} channel={channel} />
-      </ToggleContext.Provider>
-      <ToggleContext.Provider>
-        <Mute id={id} channel={channel} />
+        <Solo />
+        <Mute />
       </ToggleContext.Provider>
     </div>
   );
