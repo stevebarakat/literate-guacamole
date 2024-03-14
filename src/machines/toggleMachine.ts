513fee095a5e6fa69ccca7e58faa98cf1162ccf2
Steve Barakat <stevebarakat@gmail.com>
2024-03-14 03:33:20 -0400

    Auto commit from Stately Studio

--- src/machines/toggleMachine.ts
+++ src/machines/toggleMachine.ts
@@ -2,7 +2,8 @@ import { createActorContext } from "@xstate/react";
 import { createMachine } from "xstate";
 
 export const toggleMachine = createMachine({
-  id: "toggle",
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswFkCGAxgBYCWAdmAHTmHIkBuYAxACoDyA4hwDICiA2gAYAuolAAHVLBJ1UZMSAAeiAIwAOAKwAaEAE9VawQF8jOtBmz5i5KrQbN2XPkNFIQk6bPlvlCddr1EAE5jEx0yVAg4BXMsXEJSCgUPGRI5BV8VACYdfT8ssJBYywSbajI7RmSpVPSfRABmLIB2XOCANkLi+OsKSkqwas8071BfdsE2hCDOkyMgA */
+  id: "toggleMachine",
   initial: "active",
   states: {
     inactive: { on: { TOGGLE: "active" } },
